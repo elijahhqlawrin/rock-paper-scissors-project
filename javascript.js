ac419920b10e147c46632934d406194bb37d0787
Elijah L <129173452+elijahhqlawrin@users.noreply.github.com>
2023-04-18 16:03:13 -0300

    Add initial problem-solving comments to plan ahead

---
+++ javascript.js
@@ -0,0 +1,16 @@
+/**
+ Write a function that can determine the winner when given two inputs, one from each player.
+    Rock-rock, rock-paper, rock-scissors, paper-paper, paper-scissors, scissors-scissors, 
+ Begin round.
+    Each round should be run by a funciton called playRound with two parameters (playerSelection, computerSelection).
+        The playerSelection parameter should be case-insensitive.
+    Use prompt() to ask user to select a weapon: rock, paper, or scissors.
+        User types in weapon of choice and hits enter. Choice is returned to function.
+    Computer inputs a random weapon of choice. Use with a function called getComputerChoice that will return either a rock, paper, or scissors.
+        Return a string annuoncing the winner of the round: "You win/lose! ___ beats ___"
+            If tie, make no changes to score.
+ Begin next round.
+    Repeat lines 5-9 for a total of five rounds.
+        Announce winner/loser after final round.
+ Nest round funciton inside another function called game().
+ */
