7119cc4136d687bd3384807322b4959fbc290c61
Elijah L <129173452+elijahhqlawrin@users.noreply.github.com>
2023-04-19 08:59:24 -0300

    Wipe all comments and code, brainstorm new plan
    Outline new plan in comments.

--- javascript.js
+++ javascript.js
@@ -1,42 +1,18 @@
-/**
- Write a function that can determine the winner when given two inputs, one from each player.
-    Rock-rock, rock-paper, rock-scissors, paper-paper, paper-scissors, scissors-scissors, 
- Begin round.
-    Each round should be run by a funciton called playRound with two parameters (playerSelection, computerSelection).
-        The playerSelection parameter should be case-insensitive.
-    Use prompt() to ask user to select a weapon: rock, paper, or scissors.
-        User types in weapon of choice and hits enter. Choice is returned to function.
-    Computer inputs a random weapon of choice. Use with a function called getComputerChoice that will return either a rock, paper, or scissors.
-        Return a string annuoncing the winner of the round: "You win/lose! ___ beats ___"
-            If tie, make no changes to score.
- Begin next round.
-    Repeat lines 5-9 for a total of five rounds.
-        Announce winner/loser after final round.
- Nest round funciton inside another function called game().
- */
-
- const gameChoices = [
-    "rock",
-    "paper",
-    "scissors"
- ];
-
- function getComputerChoice() {
-    let choice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
-    return choice;
- }
-
-getComputerChoice();
-
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection > computerSelection) {
-        return "You win!";
-    } else if (computerSelection > playerSelection) {
-        return "You lose!";
-    } else {
-        return "Tie";
-    }   
-}
-
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
+ /** Create an array called gameChoices[] containing three elements: "rock", "paper", and "scissors". */
+ /** Create a function called playRound() that triggers the start of a new round.
+ Give it two parameters, playerSelection and computerSelection. */
+    /** Create a prompt() that asks player to choose between rock, paper, or scissors. */
+        /** Pass input from prompt() to the parameter playerSelection.
+         Make playerSelection case-insensitive so that the player can input "rock", "ROCK", "RocK", or any other variation. */
+    /** Create a function called getComputerChoice that tells computer to
+     randomly select one of the three elements from the array gameChoices[]. */
+        /** Pass computer's random choice to the parameter computerSelection. */
+    /** Set an if else statement that evaluates playerSelection against computerSelection. */
+        /** For brevity, let playerSelection = a, and let computerSelection = b */
+            /** If (a === "rock" && b === "rock" || a === "paper" && b === "paper" || a === "scissors" && b === "scissors")
+             return ""Tie" */
+            /** If (a === "rock" && b === "scissors" || a === "paper" && b === "rock" || a === "scissors" && b === "paper")
+             return ""You win the round!" */
+            /** If (a === "rock" && b === "paper" || a === "paper" && b === "scissors" || a === "scissors" && b === "rock")
+             return ""You lose the round." */
+ /** Create a funtion called game() that invokes the function playRound() five times. */
