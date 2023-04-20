9aff76172cc32a9b134ea90f75e8f6e515bfa4d3
Elijah L <129173452+elijahhqlawrin@users.noreply.github.com>
2023-04-20 08:48:44 -0300

    Rewrite all code from scratch; complete game

--- javascript.js
+++ javascript.js
@@ -1,10 +1,21 @@
+/** Create an array containing three elements: rock, paper, and scissors. */
 const gameChoices = ["rock", "paper", "scissors"];
 
+/** Write a function that selects a random number between 0 and 1
+ * rounds down to the nearest number
+ * and multiplies it by the number of elements in the array gameChoices. */
 function getComputerChoice() {
     let computerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
     return computerChoice;
 }
 
+/** Write a function that checks the winner of a game round.
+ * Give it two paramenters: a, b.
+ * Set conditions for parameters.
+ *  If a = b, return "tie".
+ *  If a > b in any case, return "human".
+ *  If a < b in any case, return "computer".
+ */
 function evaluateRound(humanSelection, computerSelection) {
     if (humanSelection == computerSelection) {
         return "Tie";
@@ -19,7 +30,14 @@ function evaluateRound(humanSelection, computerSelection) {
     }
 }
 
-
+/** Write a function controls a game round.
+ * Pass the same two parameter from earlier.
+ * Asign the previous function to a new variable result.
+ * Write an if else statement.
+ *  If result = "tie", return string "tie game.".
+ *  Else if result = "human", return string "human wins" and specify which choice beats which.
+ *  Else if result = "computer", return string "computer wins" and specify which choice beats which.
+ */
 function playRound(humanSelection, computerSelection) {
     const result = evaluateRound(humanSelection, computerSelection);
     if (result == "Tie") {
@@ -31,6 +49,12 @@ function playRound(humanSelection, computerSelection) {
     }
 }
 
+/** Write a function that shows the human player an input field.
+ * Prompt player to make a selection between rock, paper, or scissors.
+ * If input field is left empty, repeat prompt until given text.
+ * Convert player input to lowercase.
+ * If input field is given text, then return input.
+*/
 function getHumanChoice() {
     let humanInput = false;
     while (humanInput == false) {
@@ -46,6 +70,15 @@ function getHumanChoice() {
     }
 }
 
+/** Write a function to run all game functions.
+ * Create two variables that track the scores of the human and computer players, and initialize at 0.
+ * Write a for loop that increments either score depending on the seults of each round played.
+ * Set the loop to repeat 5 times.
+ * Signal when the game is over by logging "end game" after 5 rounds.
+ * If human player score > computer score, log "human player wins".
+ * If human player score < computer score, log "computer wins".
+ * If human player score = computer score, log "tie game".
+*/
 function game() {
     let humanScore = 0;
     let computerScore = 0;
@@ -71,4 +104,5 @@ function game() {
     }
 }
 
+/** Invoke function game() */
 game()
