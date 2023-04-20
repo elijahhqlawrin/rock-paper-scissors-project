24b1f936a29c7848acda859b3f02931d41cd956a
Elijah L <129173452+elijahhqlawrin@users.noreply.github.com>
2023-04-20 08:20:34 -0300

    Rewrite all code from scratch; complete game.
    Create an array with three elements: rock, paper, scissors.

--- javascript.js
+++ javascript.js
@@ -1,48 +1,74 @@
- /** Create an array called gameChoices[] containing three elements: "rock", "paper", and "scissors". */
- const gameChoices = ["rock", "paper", "scissors"];
- /** Create a function called playRound() that triggers the start of a new round.
- Give it two parameters, playerSelection and computerSelection. */
- function playRound() {
-    /** Create a prompt() that asks player to choose between rock, paper, or scissors. */
-    let yourWeapon = prompt("Select your weapon");
-    /** Make playerSelection case-insensitive so that the player can input "rock", "ROCK", "RocK", or any other variation. */
-    return yourWeapon.toLowerCase();
- }
-    /** Create a function called getComputerChoice that tells computer to
-     randomly select one of the three elements from the array gameChoices[]. */
-     function getComputerChoice() {
-        let computerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
-        /** Pass computer's random choice to the parameter computerSelection. */
-        return computerChoice;
-     }
+const gameChoices = ["rock", "paper", "scissors"];
 
-         /** Set an if else statement that evaluates yourWeapon against computerChoice. */
-     function roundEvaluation(yourWeapon, computerChoice) {
-            /** Set first set of conditions for player win. */
-        if (yourWeapon === "rock" && computerChoice === "scissors") {
-            return "You win the round!";
-            /** Set second set of conditions for player win. */
-        } else if (yourWeapon === "paper" && computerChoice === "rock") {
-            return "You win the round.";
-                /** Set third set of conditions for player win. */
-        } else if (yourWeapon === "scissors" && computerChoice === "paper") {
-            return "You win the round.";
-                /** Set Conditions for a "Tie" */
-        } else if (yourWeapon === computerChoice) {
-            return "Tie";
-        } else {
-            return "You lose the round."
+function getComputerChoice() {
+    let computerChoice = gameChoices[Math.floor(Math.random() * gameChoices.length)];
+    return computerChoice;
+}
+
+function evaluateRound(humanSelection, computerSelection) {
+    if (humanSelection == computerSelection) {
+        return "Tie";
+    } else if ((humanSelection == "rock" && computerSelection == "scissors") ||
+        (humanSelection == "scissors" && computerSelection == "paper") ||
+        (humanSelection == "paper" && computerSelection == "rock")
+    ) {
+        return "Human";
+    }
+    else {
+        return "Computer";
+    }
+}
+
+
+function playRound(humanSelection, computerSelection) {
+    const result = evaluateRound(humanSelection, computerSelection);
+    if (result == "Tie") {
+        return "Tie Game."
+    } else if (result == "Human") {
+        return `Human Wins! ${humanSelection} defeats ${computerSelection}`
+    } else {
+        return `Human Wins! ${computerSelection} defeats ${humanSelection}`
+    }
+}
+
+function getHumanChoice() {
+    let humanInput = false;
+    while (humanInput == false) {
+        const humanChoice = prompt("Choose your weapon: Rock, Paper, or Scissors.");
+        if (humanChoice == null) {
+            continue;
+        }
+        const humanChoiceUncapitalize = humanChoice.toLowerCase();
+        if (gameChoices.includes(humanChoiceUncapitalize)) {
+            humanInput = true;
+            return humanChoiceUncapitalize;
         }
     }
- /** Create a funtion called game() that invokes the function playRound() five times. */
+}
 
-/**
- *   if (a === "rock" && b === "scissors" || a === "paper" && b === "rock" || a === "scissors" && b === "paper") {
-            return "You win the round!";
- *    } else if (a === "rock" && b === "paper" || a === "paper" && b === "scissors" || a === "scissors" && b === "rock") {
-            return "You lose the round.";
- *    } else if (a === b === "rock" || a === b === "paper" || a === b === "scissors") {
-            return "Tie";
+function game() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(humanSelection, computerSelection));
+        console.log("<><><><><><><><><><><>");
+        if (evaluateRound(humanSelection, computerSelection) == "Human") {
+            humanScore++;
+        } else if (evaluateRound(humanSelection, computerSelection) == "Computer") {
+            computerScore++;
         }
-*/
+    }
+    console.log("End Game")
+    if (humanScore > computerScore) {
+        console.log("Human player defeated the Computer. Human player wins!");
+    } else if (computerScore > humanScore) {
+        console.log("Computer defeated the Human player. Computer wins!");
+    } else {
+        console.log("Human player and Computer are evenly matched. Neither wins.")
+    }
+}
 
+game()
